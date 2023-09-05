8ef62e90658af8cb7d8394319f8b203d1fcf1f9a
Artigupta18 <110694314+Artigupta18@users.noreply.github.com>
2023-09-05 13:37:30 +0530

    First Cahnge

--- src/com/java/testds/Studentservice.java
+++ src/com/java/testds/Studentservice.java
@@ -1,5 +1,17 @@
 package com.java.testds;
 
-public class Studentservice {
+import java.com.testds.Student;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Studentservice {
+  
+    public List<Student> displayStu(){
+    List<Student> List=new ArrayList<>();
+    List.add(new Student(1,"Test",0)); 
+    List.add(new Student(2,"Rest",0));
+    List.add(new Student(3,"Vest",0));
+    List.add(new Student(4,"Nest",0));
+    return List;
+    }
 }
--- src/com/java/testds/tester.java
+++ src/com/java/testds/tester.java
@@ -1,5 +1,25 @@
 package com.java.testds;
 
-public class tester {
+import java.com.testds.Student;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+class Mycomp implements Comparator<Student>{
+
+	@Override
+	public int compare(Student o1,Student o2) {
+		int x=o1.name.compareTo(o2.name);
+		return x;
+	}
+}
+public class tester {
+   public static void main(String[] args) {
+	   Studentservice service = new Studentservice();
+	   List<Student> List=service.displayStu();
+	   System.out.println(List);
+	   Collections.sort(List,new Mycomp());
+	   System.out.println(List);
+	   
+   }
 }
